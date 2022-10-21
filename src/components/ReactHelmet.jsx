de7f27f4794420be6796d2b8ffad93039ef10eaf
hayat <hayat@hayatui-MacBookPro.local>
2022-10-21 19:35:16 +0900

    meta image 삭제

--- src/components/ReactHelmet.jsx
+++ src/components/ReactHelmet.jsx
@@ -6,7 +6,7 @@ function ReactHelmet({ title }) {
       <meta charSet="utf-8" />
       <title>{title} | 청춘온 관리자페이지</title>
       <meta name="og:title" content={title} />
-      <meta name="og:image" content="../assets/ch-logo.gif" />
+      {/* <meta name="og:image" content="../assets/ch-logo.gif" /> */}
       <meta
         name="description"
         content="치매 예방 글쓰기 앱 플랫폼, 청춘온 관리자페이지"
